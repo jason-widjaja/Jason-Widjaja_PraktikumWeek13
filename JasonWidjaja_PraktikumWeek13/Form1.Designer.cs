5020e59ced7aa63049e76474b97eaf434b4f0eec
jason-widjaja <75228907+jason-widjaja@users.noreply.github.com>
2020-12-11 09:43:41 +0700

    Add files via upload

---
+++ JasonWidjaja_PraktikumWeek13/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Tombol = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Input = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Tombol
+            // 
+            this.Tombol.Location = new System.Drawing.Point(71, 135);
+            this.Tombol.Name = "Tombol";
+            this.Tombol.Size = new System.Drawing.Size(100, 25);
+            this.Tombol.TabIndex = 1;
+            this.Tombol.Text = "Proses";
+            this.Tombol.UseVisualStyleBackColor = true;
+            this.Tombol.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input Data:";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(71, 100);
+            this.Input.MaximumSize = new System.Drawing.Size(900, 900);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(130, 20);
+            this.Input.TabIndex = 3;
+            this.Input.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(71, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "[EMPTY]";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.UseCompatibleTextRendering = true;
+            this.label2.UseWaitCursor = true;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 361);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Tombol);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Tombol;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ JasonWidjaja_PraktikumWeek13/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (Input.Text == "HIDE")
+            {
+                label2.Visible = false;
+            }
+
+            else if (Input.Text == "SHOWN")
+            {
+                label2.Visible = true;
+            }
+
+            else if (Input.Text == "DELETE")
+            {
+                label2.Text = "[EMPTY]";
+            }
+            else if (Input.Text == "GREEN")
+            {
+                label2.ForeColor = Color.Green;
+            }
+
+            else if (Input.Text == "BLUE")
+            {
+                label2.ForeColor = Color.Blue;
+            }
+
+            else if (Input.Text == "RED")
+            {
+                label2.ForeColor = Color.Red;
+            }
+
+            else if (Input.Text != "HIDE" || Input.Text != "SHOWN")
+            {
+                label2.Text = Input.Text;
+            }
+
+
+            if (label2.Text == "[EMPTY]")
+            {
+                label2.ForeColor = Color.Black;
+            }
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+            
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
